09caac3cf7ee1db2a36ebf69f571ded15ff2e5bf
Ana Santana <123104984+Anaasca07@users.noreply.github.com>
2023-02-23 21:18:43 -0300

    Create Questão 2.c

---
+++ estrutura de dados I/ponteiros/Questão 2.c
@@ -0,0 +1,11 @@
+int  principal ( vazio ) {
+intx, *p; // declarou uma variavel do tipo inteiro e um ponteiro
+x = 100 ; // x recebe o valor 100
+p = x; // essa linha contém um erro, deveria ser p = &x;
+printf (“Valor de p = %p\tValor de *p = %d”, p, *p);
+}
+// a) a mensagem é de erro
+// b) Por causa do endereçamento p = x; que deveria ser p = &x para conseguir receber o endereço da variável
+// c) não
+// d) não, pois ainda possui um erro no endereçamento em p = x;
+// e) depois de fazer as modificações, consegui uma execução bem sucedida.
